7e8b760ee4047da090fca2bd8ccebfa78c006706
Jacob Evans <ade1987+aadev@pm.me>
2025-10-03 21:41:35 -0400

    Expanding models

--- shoppingcart/models.py
+++ shoppingcart/models.py
@@ -4,8 +4,12 @@ Create your models in here
 """
 
 # Django
+import timedate
 from django.db import models
-
+from django.utils import timezone
+from allianceauth.authentication.models import CharacterOwnership
+from allianceauth.eveonline.models import EveCharacter
+from app_utils.django import users_with_permission
 
 class General(models.Model):
     """Meta model for app permissions"""
@@ -18,5 +22,20 @@ class General(models.Model):
         permissions = (("basic_access", "Can access this app"),
                        ["shopper", "Can take orders from the cart"])
 
-class TimeRequested(models.Model):
-    timerequested = 
+class ShoppingCart(models.Model):
+    timerequested = models.DateTimeField("Time Requested")
+    requester = models.ForeignKey(
+        EveCharacter
+    )
+    destination = models.CharField(
+        max_length=20
+    )
+    janice_link = models.CharField(
+        max_length=40
+    )
+    markup = models.FloatField(
+
+    )
+    shopper = models.ForeignKey(
+        EveCharacter
+    )
